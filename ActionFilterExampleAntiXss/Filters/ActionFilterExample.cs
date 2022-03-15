cdf32c8153aa7020466feddb7ca4d1fb223e5568
Alihan Girgin <alihangir@gmail.com>
2022-03-16 01:08:05 +0300

    Add project files.

---
+++ ActionFilterExampleAntiXss/Filters/ActionFilterExample.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.Security.Application;
+
+namespace ActionFilterExampleAntiXss.Filters
+{
+    public class ActionFilterExample : IActionFilter
+    {
+        public void OnActionExecuting(ActionExecutingContext context)
+        {
+            // our code before action executes
+            var stringArgs = context.ActionArguments.Where(pair => pair.Value is object).ToList();
+
+            foreach (var keyValue in stringArgs)
+            {
+                foreach (var item in keyValue.Value.GetType().GetProperties())
+                {
+                    var unsafeValue = item.GetValue(keyValue.Value);
+                    if (unsafeValue is string)
+                    {
+                        var safeValue = Encoder.HtmlEncode((string)unsafeValue);
+                        item.SetValue(keyValue.Value, safeValue);   
+                    }
+                }
+            }
+        }
+        public void OnActionExecuted(ActionExecutedContext context)
+        {
+            // our code after action executes
+        }
+    }
+}
---
+++ ActionFilterExampleAntiXss/Models/NewCustomerViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace ActionFilterExampleAntiXss.Models
+{
+    public class NewCustomerViewModel
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public bool IsDeleted { get; set; }
+        public int CustomerId { get; set; }
+    }
+}
